65414972f9f4a7de167c877e4c5b586f8fdf6bdb
Ashish011998 <38317616+Ashish011998@users.noreply.github.com>
2018-04-28 18:52:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,20 @@ request.onreadystatechange = function(){
 request.open('GET', 'http://ashishpatidar01.imad.hasura-app.io/counter',true);
 request.send('null');
 };
+
+
+//sumit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submmit-btn');
+submit.onclick = function(){
+  // should make a request to a server
+  //capture list of name and render the names as list
+  var names = ['name1','name2','name3'];
+  var list = '';
+  for (var i=0; i<names.length; i++){
+      list+='<li>'+names[i]'</li>';
+  }
+  var ul = document.getElementById('nameList');
+  ul.innerHTML = list;
+};
